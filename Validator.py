042250b43dc796abc3e8b2d4462a2bc8d84f82f8
Nicko Cruz M <nickocruzm@gmail.com>
2023-05-27 19:30:59 -0700

    Part_2 Submission

--- Validator.py
+++ Validator.py
@@ -1,65 +1,30 @@
 from Node import Node
-import math
+from NN_Classifier import *
 
+
+
+class Validator:
     
-def majority_vote(a, nearest_neighbors):
-    total_votes = len(nearest_neighbors)
-    class_1_votes = 0
-    for n in nearest_neighbors:
-        node = n['node']
-        if(float(1.0) == node.TrueLabel):
-            class_1_votes += 1
-        
-    class_2_votes = (total_votes - class_1_votes)
-    if(class_1_votes > class_2_votes):
-        return float(1.0)
-    elif(class_2_votes > class_1_votes):
-        return float(2.0)
-    else:
-        print("TIE")
-        return(-1)
-
-def Determine_Accuracy(NODES, feature_subset, k):
-        accurate_computations = 0
-        for i in range(0,len(NODES)):
-            for j in range(i,len(NODES)):
-                if i == j:
-                    continue
-                else:
-                    nearest_neighbors = NODES[i].get_nearest_neighbors(k)
-                    computed_Lbl = majority_vote(NODES[i],nearest_neighbors)
-                    NODES[i].ComputedLabel = computed_Lbl
-                    
-        
-            if(computed_Lbl == NODES[i].TrueLabel):
-                accurate_computations += 1
-        
-        return accurate_computations/(len(NODES))
+    @classmethod
+    def Determine_Accuracy(cls,NODES, feature_subset, k):
+            if(len(NODES[0].neighbors) == 0):
+                NN_Classifier.compute_distances(NODES,feature_subset)
             
             
-        
-        
-
-                    
-                    
+            accurate_computations = 0
+            for i in range(0,len(NODES)):            
+                for j in range(i,len(NODES)):
+                    if i == j:
+                        continue
+                    else:
+                        nearest_neighbors = NODES[i].get_nearest_neighbors(k)
                         
+                        computed_Lbl = NN_Classifier.majority_vote(NODES[i],nearest_neighbors)
+                        NODES[i].ComputedLabel = computed_Lbl
                         
-                        
-                        
-                        
-                        
-                    
-                    
-                    
-                    
-                    # try:
-                    #     euclidean_dist(node, neighbor)
-                        
-                    # except IndexError:
-                    #     print("...evaluating neighbors distance...")
-
-                    
-                    # finally:
-                    #     print("...computing accuracy...")
+                if(computed_Lbl == NODES[i].TrueLabel):
+                    accurate_computations += 1
+            
+            return accurate_computations/(len(NODES))
    
     
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from data_handler import *
+from Data import *
 from Validator import *
-from Classifier import *
+from NN_Classifier import *
 from Node import Node
+
+# outputs Trace by iterating through a list of node objects. Retrieving the node.id attribute, and nodes.neighbor that is at the top of list.
+# Actual Accuracy is computed in Validator.py
+def Accuracy_Trace(NODES):
+    for node in NODES:
+        print(f'object {node.id} is class {node.TrueLabel}')
+        print(f"nearest_neighbor(s): {node.neighbors[0]['node'].id}, which is class {node.neighbors[0]['node'].TrueLabel}")
 
 if __name__ == '__main__':
     fileName = input("filename: ")
@@ -13,35 +20,25 @@ if __name__ == '__main__':
     # Pass in feature subset wanted
     number_of_features = len(NODES[0].features)
     print(f'Features: 1 to {number_of_features}')
-    print('input nums seperated by spaces of features to use for classification: EX) -> 2 5 7')
-    print('if no input is given, classifier will default to using all features')
+    print('\t input nums seperated by spaces of features to use for classification: EX) -> 2 5 7')
+    print('\t if no input is given, classifier will default to using all features')
     feature_subset_str = input("Feature_subset: ")
     feature_subset = [int(x) for x in feature_subset_str.split()]
     
-    print('how many (k)-neighbors would like to be used for classification: ')
+    
+    print('\t how many (k)-neighbors would like to be used for classification')
     k = int(input('k_neighbors: '))
-    
-    compute_distances(NODES,feature_subset)
-    
-    
     
     
     # Accuracy
-    accuracy = Determine_Accuracy(NODES,feature_subset,k)
-    print(f'accuracy of {k}-Nearest Neighbors Classifier, using {len(feature_subset)} features: {accuracy*100}%')
+    accuracy = Validator.Determine_Accuracy(NODES,feature_subset,k)
+    print(f'\t accuracy of {k}-Nearest Neighbors Classifier, using features {feature_subset} features: {accuracy*100}%')
     
-    # classify
+    #Accuracy_Trace(NODES)
+    
+    #classify
     print(f"insert ID of instance you'd like to classify from 1 to {len(NODES)}")
-    # ID = int(input('id: '))
-    
-    # if(NODES[ID].ComputedLabel != None):
-    
-    
-    
-    
-
-        
+    id_num = int(input('id: '))
+    NN_Classifier.classify(id_num,NODES,feature_subset,k)
+  
  
-    
-    
-    
