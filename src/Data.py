5f6ce66e700b7ec62e80f5982fb682601b5cf48d
Nicko Cruz M <nickocruzm@gmail.com>
2023-08-08 12:41:44 -0700

    Update Data.py

--- src/Data.py
+++ src/Data.py
@@ -12,21 +12,7 @@ def read_data(fileName):
     return matrix
 
 def Data_to_Nodes(raw_data):
-    NODES = [Node(n) for n in raw_data]
-    return NODES
-
-# def find_min_max(matrix):
-#     num_rows = len(matrix)
-#     num_cols = len(matrix[0])
-
-#     min_values = [float('inf')] * num_cols
-#     max_values = [float('-inf')] * num_cols
-
-#     for i in range(num_rows):
-#         for j in range(1,num_cols):
-#             if matrix[i][j] < min_values[j]: min_values[j] = matrix[i][j]
-#             if matrix[i][j] > max_values[j]: max_values[j] = matrix[i][j]
-#     return min_values, max_values
+    return [Node(n) for n in raw_data]
 
 def default_accuracy(raw_data):
     A = 0
@@ -40,9 +26,7 @@ def default_accuracy(raw_data):
     return(most_frequent / total_instances)
 
 def normalize_data(raw_data):
-    
     def min_max_normalization(data):
-        
         def find_min_max(matrix):
             num_rows = len(matrix)
             num_cols = len(matrix[0])
@@ -71,5 +55,4 @@ def get_nearest_neighbors(distances_memory, instance):
     """
         for the distances in memory, retrieve the minimum distance in relative to the given instance
     """
-    
     return min(distances_memory[instance])
