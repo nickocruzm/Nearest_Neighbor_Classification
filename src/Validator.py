f4a11461bf9f94a9151facdbff96b9e492f507c6
Nicko Cruz M <nickocruzm@gmail.com>
2023-08-23 02:41:02 -0700

    style: Header file

--- src/Validator.py
+++ src/Validator.py
@@ -3,27 +3,23 @@ import math
 from NN_Classifier import *
 
 
-
-class Validator:
+def leave_one_out_cross_validation(Nodes,feature_subset,new_feature:int = -1):
+    nearest_neighbor = None
+    minimum_distance = float('inf')
+    accuracy = 0
+    total_instances = len(Nodes)
+    
+    if new_feature != -1:
+        new_subset = feature_subset.copy()
+        new_subset.append(new_feature)
+    else:
+        new_subset = feature_subset.copy()   
+    NN_Classifier.compute_distances(Nodes, new_subset)
+    accuracy = Validator.Determine_Accuracy(Nodes)
+    return accuracy
     
     @classmethod
-    def leave_one_out_cross_validation(cls,Nodes,feature_subset,new_feature:int = -1):
-        nearest_neighbor = None
-        minimum_distance = float('inf')
-        accuracy = 0
-        total_instances = len(Nodes)
-        
-        if new_feature != -1:
-            new_subset = feature_subset.copy()
-            new_subset.append(new_feature)
-        else:
-            new_subset = feature_subset.copy()   
-        NN_Classifier.compute_distances(Nodes, new_subset)
-        accuracy = Validator.Determine_Accuracy(Nodes)
-        return accuracy
-    
-    @classmethod
-    def Determine_Accuracy(cls,Nodes):
+    def Determine_Accuracy(Nodes):
         """
             Determines accuracy using only the features given in the feature_subset
             on the data set
