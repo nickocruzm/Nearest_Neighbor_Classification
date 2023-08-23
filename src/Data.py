a954c26398ab588a058361e12bb9d132b46a437a
Nicko Cruz M <nickocruzm@gmail.com>
2023-08-23 06:05:50 -0700

    closures removed

--- src/Data.py
+++ src/Data.py
@@ -38,9 +38,25 @@ def default_accuracy(raw_data) -> float:
     most_frequent = max(A,B)
     return(most_frequent / total_instances)
 
-def normalize_data(raw_data):
-    
-    def min_max_normalization(data):
+def find_min_max(matrix):
+    """       
+        return 2 arrays, of the min and max values for each column.
+    """
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+
+    # containers to hold the minmums and maximums of each column
+    min_values = [float('inf')] * num_cols
+    max_values = [float('-inf')] * num_cols
+
+    # Find the min and max of each col, nested loop skips j=0, because that field contains the true instance's classification.
+    for i in range(num_rows):
+        for j in range(1,num_cols):
+            if matrix[i][j] < min_values[j]: min_values[j] = matrix[i][j]
+            if matrix[i][j] > max_values[j]: max_values[j] = matrix[i][j]
+    return min_values, max_values
+
+def min_max_normalization(data):
         """
             1st closure contained in normalize_data()
             using the minimum values and maximum values returned from find_min_max(),
@@ -48,25 +64,7 @@ def normalize_data(raw_data):
             
             returns dataset, with normalized data
         """
-        def find_min_max(matrix):
-            """
-                2nd clousure contained in normalize_data()
-                
-                return 2 arrays, of the min and max values for each column.
-            """
-            num_rows = len(matrix)
-            num_cols = len(matrix[0])
-            
-            # containers to hold the minmums and maximums of each column
-            min_values = [float('inf')] * num_cols
-            max_values = [float('-inf')] * num_cols
-            
-            # Find the min and max of each col, nested loop skips j=0, because that field contains the true instance's classification.
-            for i in range(num_rows):
-                for j in range(1,num_cols):
-                    if matrix[i][j] < min_values[j]: min_values[j] = matrix[i][j]
-                    if matrix[i][j] > max_values[j]: max_values[j] = matrix[i][j]
-            return min_values, max_values
+       
             
         mins,maxes = find_min_max(data)
         num_of_features = len(maxes)
@@ -79,7 +77,8 @@ def normalize_data(raw_data):
                 data[j][i] = (data[j][i] - mins[i])/(maxes[i] - mins[i])
         
         return data
-    
+
+def normalize_data(raw_data):
     return min_max_normalization(raw_data)
 
 def get_nearest_neighbors(distances_memory, instance):
