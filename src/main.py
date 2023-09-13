33a1490abcf8fdfa3762ab5fdd014ddc874aeecf
Nicko Cruz M <nickocruzm@gmail.com>
2023-09-13 14:55:14 -0700

    userPrompt: change

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from Validator import *
 from Search import *
 
 if __name__ == '__main__':
-    fileName = input("filename: "); filePath = "datasets/" + fileName
+    fileName = input("\n filename: "); filePath = "datasets/" + fileName
     
     raw_data = read_data(filePath)
     
